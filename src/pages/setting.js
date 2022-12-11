5dd142a3aac99fd943d31952e590d166d2aa4d6f
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-12-11 13:12:28 +0000

    update settings

--- src/pages/setting.js
+++ src/pages/setting.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
+import "../styles/settings.css";
 
 function setting() {
   return (
-    <div>
+    <div className="settings">
+      <div className="settings_wrapper">
+        <h2 className="settings_title">Settings</h2>
+        <div className="settings_top">
+          <button className="setting_btn">My Details</button>
+          <button className="setting_btn active_btn">Profile</button>
+          <button className="setting_btn">Passwords</button>
+          <button className="setting_btn">Email</button>
+          <button className="setting_btn">Notifications</button>
+        </div>
+        <div className="details_form">
+          <h2 className="profile">Profile</h2>
+          <p className="profile_desc">
+            Update your photo and personnal details
+          </p>
 
+          <form>
+            <div className="form_group">
+              <div>
+                <label>Live In</label>
+                <input type="text" placeholder="England" />
+              </div>
+              <div>
+                <label>Street</label>
+                <input type="text" placeholder="placeholder" />
+              </div>
+            </div>
+            <div className="form_group">
+              <div>
+                <label>Email</label>
+                <input type="email" placeholder="example@gmail.com" />
+              </div>
+              <div>
+                <label>Phone Number</label>
+                <input type="number" placeholder="07*********" />
+              </div>
+            </div>
+            <div className="form_group">
+              <div>
+                <label>Date of Birth</label>
+                <input type="date" placeholder="dd/mm/yyyy" />
+              </div>
+              <div>
+                <label>Gender</label>
+                <input type="text" placeholder="Female" />
+              </div>
+            </div>
+            <div className="form_group">
+              <div>
+                <label>Profile Photo</label>
+                <p className="profile-img_desc">This will be displayed</p>
+                <input type="file" placeholder="choose file" />
+              </div>
+
+              <div className="profile_img-btns">
+                <button className="dlt_btn"></button>
+                <button className="update_btn"></button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default setting
+export default setting;
